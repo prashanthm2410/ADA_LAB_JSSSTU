ad7ea5e480b62cde9cc4ed2a0baccbb5ee97fbc8
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-07-30 12:40:49 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -1,70 +1,84 @@
 #include<stdio.h>
 #include<stdlib.h>
-int graph[50][50],visited[50],path[50],d,isCyclic;
-int count,dfscall;
+int visited[50],path[50],d,graph[50][50],iscyclic,count,dfscall,graphcount;
 void dfs(int n,int start,int parent)
 {
-    visited[start]=1;
-    path[start]=1;
+    visited[start]=1; path[start]=1;
     count++;
-    printf("%c---->",start+65);
+    //printf("%c---->",start+65);
     for(int i=0;i<n;i++)
     {
         if(d==1)
         {
             if(i!=parent && visited[i] && graph[start][i] && path[i])
-            isCyclic=1; 
+            iscyclic=1;
         }
         else
         {
             if(i!=parent && visited[i] && graph[start][i])
-            isCyclic=1;
+            iscyclic=1;
         }
+        graphcount++;
         if(visited[i]==0 && graph[start][i])
-        dfs(n,i,start);    
+        {
+            dfs(n,i,start);
+        }
     }
     path[start]=0;
 }
 void main()
 {
-    //Generate Dense and Simply Connected Graph and append to file in a+ mode for different vertices
-    //CORRECTNESS
-    int n;
-    printf("Enter the Number of Vertices : ");scanf("%d",&n);
-    printf("Enter 1 if Graph is Directed Else 0 : ");scanf("%d",&d);
+    //WHILE RUNNING FOR PLOTING COMMENT OUT THE PRINTF INSIDE THE DFS FUNCTION
+    FILE *fp=fopen("plot.txt","w");
+    for(int m=2;m<=15;m++)
+    {
+        graphcount=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+            if(i==j)
+            graph[i][j]=0;
+            else
+            graph[i][j]=1;
+            }
+        visited[i]=0;path[i]=0;
+        }
+        dfs(m,0,-1);
+        fprintf(fp,"%d\t\t%d\n",m,graphcount);
+    }
+    fclose(fp);
+    /*printf("Enter the Number of vertices : ");int n,start;scanf("%d",&n);
+    printf("Enter 1 if the Graph is Directed else 0 :");scanf("%d",&d);
     printf("Enter the Adjacency Matrix\n");
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
-        {
-            scanf("%d",&graph[i][j]);
-        }
-        visited[i]=0;
-        path[i]=0;
+        scanf("%d",&graph[i][j]);
+        visited[i]=0;path[i]=0;
     }
-    isCyclic=0;
     dfs(n,0,-1);
     dfscall++;
-    if(isCyclic)
-        printf("Graph is Cyclic\n");
+    if(iscyclic)
+    printf("\nGraph is Cyclic\n");
     else
-        printf("Graph is Acyclic\n");
+    printf("\nGraph is Acyclic\n");
     if(count==n)
     printf("Graph is Connected\n");
     else
     {
         printf("Graph is NOT Connected\n");
-        int start=1;
         while(count!=n)
         {
+            start=1;
             if(visited[start]==0)
             {
-            printf("\n");
-            dfs(n,start,-1);
-            dfscall++;
+                printf("\n");
+                dfs(n,start,-1);
+                dfscall++;
             }
             start++;
         }
     }
-    printf("Number Of Connected Componenets is %d\n",dfscall);
+    printf("\nNumber of Conned Componenets is : %d\n",dfscall);*/
 }
